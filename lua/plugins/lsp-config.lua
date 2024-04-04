2f4e491ead4db8a64dc8b844cf8cd341990ad410
roesch <kevin.roesch@kit.edu>
2024-04-04 05:22:34 +0200

    add lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,11 +18,18 @@ return {
 		lazy = false,
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
-		},
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+        },
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
+            require('mason').setup()
+            local mason_lspconfig = require 'mason-lspconfig'
+            mason_lspconfig.setup {
+                ensure_installed = { "pyright" }
+            }
 			lspconfig.clangd.setup({
 				capabilities = capabilities,
 			})
