a76973a63e506f26df4dcdef41c73bb5e781a68a
Kevin Rösch <kevin.roesch@kit.edu>
2024-07-05 08:56:25 +0200

    pull

--- lua/plugins/treesiter.lua
+++ lua/plugins/treesiter.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = { "c", "python", "lua", "vim", "vimdoc", "javascript", "html" },
+        ensure_installed = { "c", "python", "lua", "vim", "vimdoc", "javascript", "html", "latex" },
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
