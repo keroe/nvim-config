cabd6c5bcff66ac210b9cc928a7fae6c7579051c
roesch <kevin.roesch@kit.edu>
2024-08-26 14:12:02 +0200

    fix format

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -2,25 +2,5 @@ return { "jose-elias-alvarez/null-ls.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local null_ls = require("null-ls")
-
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.diagnostics.ruff,
-                null_ls.builtins.formatting.black,
-            },
-            on_attach = function(client, bufnr)
-                if client.supports_method("textDocument/formatting") then
-                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = augroup,
-                        buffer = bufnr,
-                        callback = function()
-                            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                            vim.lsp.buf.format({ async = false })
-                        end,
-                    })
-                end
-            end,
-        })
     end,
 }
