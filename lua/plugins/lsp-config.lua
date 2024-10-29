6f1b810bb2a1c4f5a58b0bafa04aba95936f9dca
Kevin Rösch <kevin.roesch@kit.edu>
2024-10-29 07:56:52 +0100

    remove deprecation warning

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -36,7 +36,7 @@ return {
             lspconfig.clangd.setup({
                 capabilities = capabilities,
             })
-            lspconfig.tsserver.setup({
+            lspconfig.ts_ls.setup({
                 capabilities = capabilities,
             })
             lspconfig.html.setup({
