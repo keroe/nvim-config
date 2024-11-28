956ee6e84027c483cb767237862c9adb1443f953
Kevin Rösch <kevin.roesch@kit.edu>
2024-11-28 11:51:02 +0100

    add ignores for grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,42 @@
 return {
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
         dependencies = { "nvim-lua/plenary.nvim",
             { 'nvim-telescope/telescope-ui-select.nvim' },
 
             -- Useful for getting pretty icons, but requires a Nerd Font.
             { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
         },
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			local builtin = require("telescope.builtin")
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+                defaults = {
+                    vimgrep_arguments = {
+                        'rg',
+                        '--color=never',
+                        '--no-heading',
+                        '--with-filename',
+                        '--line-number',
+                        '--column',
+                        '--smart-case',
+                        '--ignore',         -- Respect .gitignore and other ignore files
+                        '--glob', '!log/**',
+                        '--glob', '!build/**',
+                        '--glob', '!devel/**',
+                        '--glob', '!doc/**',
+                        '--glob', '!install/**',
+                    }
+                },
+            })
+            local builtin = require("telescope.builtin")
             vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
             vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
             vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
@@ -31,7 +48,7 @@ return {
             vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
             vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+            require("telescope").load_extension("ui-select")
+        end,
+    },
 }
