08f0ac9aecc00dd4b8db273c3959be87e63fba6d
Kevin Rösch <kevin.roesch@kit.edu>
2024-12-17 17:25:10 +0100

    make update for notify

--- init.lua
+++ init.lua
@@ -12,4 +12,8 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("keroe")
-require("lazy").setup("plugins")
+require("lazy").setup({
+    spec = {{import = "plugins"},
+},
+  checker = { enabled = true },
+})
