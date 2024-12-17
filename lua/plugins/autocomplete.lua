27ce40969ef098f35eb834ee18527871b9014b13
Kevin Rösch <kevin.roesch@kit.edu>
2024-12-17 14:19:26 +0100

    faster timeout

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -41,10 +41,14 @@ return {
     },
 	{
 		"folke/which-key.nvim",
+        dependencies = {
+            "echasnovski/mini.icons",
+            "nvim-tree/nvim-web-devicons",
+        },
 		event = "VeryLazy",
 		init = function()
 			vim.o.timeout = true
-			vim.o.timeoutlen = 500
+			vim.o.timeoutlen = 200
 		end,
 		opts = {},
 	},
