525f8808b29dab71dd8f6247dc9c88d5f5092e37
Kevin Rösch <kevin.roesch@kit.edu>
2024-12-19 10:57:27 +0100

    new copilot

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,50 +1,73 @@
 return {
 	{
-		"github/copilot.vim",
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = {
+					auto_trigger = true,
+					keymap = {
+						accept = "<tab>",
+						accept_word = "<S-tab>",
+						next = "<M-]>",
+						prev = "<M-[>",
+						dismiss = "<M-e>",
+					},
+				},
+				filetypes = {
+					yaml = true,
+					markdown = true,
+					gitcommit = true,
+					gitrebase = true,
+					cvs = true,
+					["."] = true,
+				},
+			})
+		end,
 	},
-    {
-        {
-            "CopilotC-Nvim/CopilotChat.nvim",
-            branch = "main",
-            dependencies = {
-                { "github/copilot.vim" }, -- or github/copilot.vim
-                { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-            },
-            opts = {
-                debug = true, -- Enable debugging
+	{
+		{
+			"CopilotC-Nvim/CopilotChat.nvim",
+			branch = "main",
+			dependencies = {
+				{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+				{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+			},
+			opts = {
+				debug = true, -- Enable debugging
 
-                -- See Configuration section for rest
-            },
-            config = function(_, opts)
-                local chat = require("CopilotChat")
-                local select = require("CopilotChat.select")
-                chat.setup(opts)
-                require("CopilotChat.integrations.cmp").setup()
-                vim.api.nvim_create_user_command("CopilotChatInline", function(args)
-                    chat.ask(args.args, {
-                        selection = select.visual,
-                        window = {
-                            layout = "float",
-                            relative = "cursor",
-                            width = 1,
-                            height = 0.4,
-                            row = 1,
-                        },
-                    })
-                end, { nargs = "*", range = true })
-
-            end,
-            keys = {
-                { "<leader>cc", "<cmd>CopilotChatInline<cr>", desc="CopilotChat - Inline chat"},
-            }
-        },
-    },
+				-- See Configuration section for rest
+			},
+			config = function(_, opts)
+				local chat = require("CopilotChat")
+				local select = require("CopilotChat.select")
+				chat.setup(opts)
+				require("CopilotChat.integrations.cmp").setup()
+				vim.api.nvim_create_user_command("CopilotChatInline", function(args)
+					chat.ask(args.args, {
+						selection = select.visual,
+						window = {
+							layout = "float",
+							relative = "cursor",
+							width = 1,
+							height = 0.4,
+							row = 1,
+						},
+					})
+				end, { nargs = "*", range = true })
+			end,
+			keys = {
+				{ "<leader>cc", "<cmd>CopilotChatInline<cr>", desc = "CopilotChat - Inline chat" },
+			},
+		},
+	},
 	{
 		"folke/which-key.nvim",
-        dependencies = {
-            "echasnovski/mini.icons",
-            "nvim-tree/nvim-web-devicons",
-        },
+		dependencies = {
+			"echasnovski/mini.icons",
+			"nvim-tree/nvim-web-devicons",
+		},
 		event = "VeryLazy",
 		init = function()
 			vim.o.timeout = true
@@ -62,7 +85,7 @@ return {
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
-            'hrsh7th/cmp-nvim-lsp'
+			"hrsh7th/cmp-nvim-lsp",
 		},
 	},
 	{
@@ -87,22 +110,21 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-                    ["<C-j>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        else
-                            fallback()
-                        end
-                        end, {"i", "s"}),
+					["<C-j>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 
-                    ["<C-k>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        else
-                            fallback()
-                        end
-                        end, {"i", "s"}),
-
+					["<C-k>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
@@ -111,10 +133,10 @@ return {
 					{ name = "buffer" },
 				}),
 			})
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            require('lspconfig')['pyright'].setup {
-                capabilities = capabilities
-            }
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			require("lspconfig")["pyright"].setup({
+				capabilities = capabilities,
+			})
 		end,
 	},
 }
