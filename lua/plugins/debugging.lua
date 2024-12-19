c47102c66714610fe59e2034c34a1ff58f807d9e
roesch <kevin.roesch@kit.edu>
2024-12-19 10:51:28 +0100

    update debugging confiig

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -7,9 +7,7 @@
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 
 return {
-  -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
@@ -21,8 +19,6 @@ return {
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
 
-    -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
     'mfussenegger/nvim-dap-python'
   },
   config = function()
@@ -68,10 +64,6 @@ return {
     vim.keymap.set('n', '<F10>', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<F11>', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<F12>', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>d', function()
-      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-    end, { desc = 'Debug: Set Breakpoint' })
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
@@ -95,6 +87,16 @@ return {
       },
     }
 
+    -- vim.api.nvim_set_hl(0, 'DapBreakpoint', {bg='#31353f' })
+    -- vim.api.nvim_set_hl(0, 'DapLogPoint', {bg='#31353f' })
+    -- vim.api.nvim_set_hl(0, 'DapStopped', {bg='#31353f' })
+
+
+    vim.fn.sign_define('DapBreakpoint', { text='●', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+    vim.fn.sign_define('DapBreakpointCondition', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+    vim.fn.sign_define('DapBreakpointRejected', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl= 'DapBreakpoint' })
+    vim.fn.sign_define('DapLogPoint', { text='', texthl='DapLogPoint', linehl='DapLogPoint', numhl= 'DapLogPoint' })
+    vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
 
@@ -102,8 +104,6 @@ return {
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
-    -- Install golang specific config
-    require('dap-go').setup()
     require('dap-python').setup("python")
   end,
 }
