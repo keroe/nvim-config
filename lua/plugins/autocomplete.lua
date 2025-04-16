f549eef858cde1d4938bccd51b099c18cb2211a7
roesch <kevin.roesch@kit.edu>
2025-04-16 07:09:14 +0000

    remove double pyright setup

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -134,9 +134,6 @@ return {
 				}),
 			})
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			require("lspconfig")["pyright"].setup({
-				capabilities = capabilities,
-			})
 		end,
 	},
 }
