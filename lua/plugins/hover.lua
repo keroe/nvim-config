3bc4e594d4abe1cf887c388200c3dd70cdf043a8
roesch <kevin.roesch@kit.edu>
2025-05-09 06:29:01 +0000

    better hover

--- lua/plugins/hover.lua
+++ lua/plugins/hover.lua
@@ -20,7 +20,7 @@ return {
             },
             -- Whether the contents of a currently open hover window should be moved
             -- to a :h preview-window when pressing the hover keymap.
-            preview_window = false,
+            preview_window = true,
             title = true,
             mouse_providers = {
                 'LSP'
@@ -29,8 +29,8 @@ return {
         }
 
         -- Setup keymaps
-        vim.keymap.set("n", "K", require("hover").hover, {desc = "hover.nvim"})
-        vim.keymap.set("n", "gK", require("hover").hover_select, {desc = "hover.nvim (select)"})
+        -- vim.keymap.set("n", "K", require("hover").hover, {desc = "hover.nvim"})
+        -- vim.keymap.set("n", "gK", require("hover").hover_select, {desc = "hover.nvim (select)"})
         vim.keymap.set("n", "<C-p>", function() require("hover").hover_switch("previous") end, {desc = "hover.nvim (previous source)"})
         vim.keymap.set("n", "<C-n>", function() require("hover").hover_switch("next") end, {desc = "hover.nvim (next source)"})
 
