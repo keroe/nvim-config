4d383c1102f6e797b91eea37efe23258bc14f2c6
roesch <kevin.roesch@kit.edu>
2025-06-11 07:43:20 +0000

    new catpuccin

--- lua/plugins/catpuccin.lua
+++ lua/plugins/catpuccin.lua
@@ -1,52 +1,53 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  opts = {
-    term_colors = true,
-    transparent_background = true,
-    styles = {
-        comments = {"bold"},
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+        term_colors = true,
+        transparent_background = true,
+        styles = {
+            comments = { "bold" },
+        },
+        integrations = {
+            cmp = true,
+            gitsigns = true,
+            treesitter = true,
+            harpoon = true,
+            telescope = true,
+            mason = true,
+            noice = true,
+            notify = true,
+            neotree = true,
+            which_key = true,
+            fidget = true,
+            dap = true,
+            dap_ui = true,
+            native_lsp = {
+                enabled = true,
+                virtual_text = {
+                    errors = { "italic" },
+                    hints = { "italic" },
+                    warnings = { "italic" },
+                    information = { "italic" },
+                },
+                underlines = {
+                    errors = { "underline" },
+                    hints = { "underline" },
+                    warnings = { "underline" },
+                    information = { "underline" },
+                },
+                inlay_hints = {
+                    background = true,
+                },
+            },
+            mini = {
+                enabled = true,
+                indentscope_color = "",
+            },
+        },
     },
-    integrations = {
-      cmp = true,
-      gitsigns = true,
-      treesitter = false,
-      harpoon = true,
-      telescope = true,
-      mason = true,
-      noice = true,
-      notify = true,
-      which_key = true,
-      fidget = true,
-      dap = true,
-      dap_ui = true,
-      native_lsp = {
-        enabled = true,
-        virtual_text = {
-          errors = { "italic" },
-          hints = { "italic" },
-          warnings = { "italic" },
-          information = { "italic" },
-        },
-        underlines = {
-          errors = { "underline" },
-          hints = { "underline" },
-          warnings = { "underline" },
-          information = { "underline" },
-        },
-        inlay_hints = {
-          background = true,
-        },
-      },
-      mini = {
-        enabled = true,
-        indentscope_color = "",
-      },
-    },
-  },
-  config = function(_, opts)
-    require("catppuccin").setup(opts)
-    vim.cmd.colorscheme("catppuccin-mocha")
-  end,
+    config = function(_, opts)
+        require("catppuccin").setup(opts)
+        vim.cmd.colorscheme("catppuccin-mocha")
+    end,
 }
