966c44c6aef5ca6935bd177fd66796448145ad77
Kevin Rösch <kevin.roesch@kit.edu>
2025-06-11 10:12:39 +0200

    fix neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -95,7 +95,24 @@ return {
             },
         })
 
-        vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+         vim.api.nvim_create_autocmd("User", {
+            pattern = "NeoTreeBufferLeave",
+            callback = function()
+                local shown_buffers = {}
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                    shown_buffers[vim.api.nvim_win_get_buf(win)] = true
+                end
+                for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+                    if not shown_buffers[buf]
+                        and vim.api.nvim_buf_get_option(buf, 'buftype') == 'nofile'
+                        and vim.api.nvim_buf_get_option(buf, 'filetype') == 'neo-tree'
+                    then
+                        vim.api.nvim_buf_delete(buf, { force = true })
+                    end
+                end
+            end,
+        })
+
         vim.keymap.set("n", "<leader>nb", ":Neotree filesystem reveal right<CR>", {})
         vim.keymap.set("n", "<leader>nc", ":Neotree close<CR>", {})
         vim.keymap.set("n", "<leader>nf", ":Neotree buffers reveal float<CR>", {})
