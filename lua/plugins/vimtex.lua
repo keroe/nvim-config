c897ca076cce1e2f605b4781d8f0cb4afed830ef
Kevin Rösch <kevin.roesch@kit.edu>
2025-06-11 10:44:36 +0200

    change leader

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -6,7 +6,7 @@ return {
     init = function()
         -- VimTeX configuration goes here, e.g.
         vim.g.vimtex_view_general_viewer = 'zathura'
-        maplocalleader = ","
+        maplocalleader = " "
     end
     },
 }
