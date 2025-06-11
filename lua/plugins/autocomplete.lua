9e3cfb91a8d4f7fcffdcc9ff494fafdcb2334e52
roesch <kevin.roesch@kit.edu>
2025-06-11 07:43:15 +0000

    new copilot config

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,139 +1,115 @@
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		config = function()
-			require("copilot").setup({
-				suggestion = {
-					auto_trigger = true,
-					keymap = {
-						accept = "<tab>",
-						accept_word = "<S-tab>",
-						next = "<M-]>",
-						prev = "<M-[>",
-						dismiss = "<M-e>",
-					},
-				},
-				filetypes = {
-					yaml = true,
-					markdown = true,
-					gitcommit = true,
-					gitrebase = true,
-					cvs = true,
-					["."] = true,
-				},
-			})
-		end,
-	},
-	{
-		{
-			"CopilotC-Nvim/CopilotChat.nvim",
-			branch = "main",
-			dependencies = {
-				{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-				{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-			},
-			opts = {
-				debug = true, -- Enable debugging
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                suggestion = {
+                    auto_trigger = true,
+                    keymap = {
+                        accept = "<Tab>",
+                        accept_word = "<S-Tab>",
+                        next = "<M-]>",
+                        prev = "<M-[>",
+                        dismiss = "<M-e>",
+                    },
+                },
+                filetypes = {
+                    yaml = true,
+                    markdown = true,
+                    gitcommit = true,
+                    gitrebase = true,
+                    cvs = true,
+                    ["."] = true,
+                },
+            })
+        end,
+    },
+    {
+        {
+            "CopilotC-Nvim/CopilotChat.nvim",
+            dependencies = {
+                { "zbirenbaum/copilot.lua" },                   -- or zbirenbaum/copilot.lua
+                { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+            },
+            build = "make tiktoken",                            -- Only on MacOS or Linux
+            opts = {
+                -- See Configuration section for options
+            },
+            -- See Commands section for default commands if you want to lazy load on them
+        },
+    },
+    {
+        "folke/which-key.nvim",
+        dependencies = {
+            "echasnovski/mini.icons",
+            "nvim-tree/nvim-web-devicons",
+        },
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 200
+        end,
+        opts = {},
+    },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = true,
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            local cmp = require("cmp")
+            require("luasnip.loaders.from_vscode").lazy_load()
 
-				-- See Configuration section for rest
-			},
-			config = function(_, opts)
-				local chat = require("CopilotChat")
-				local select = require("CopilotChat.select")
-				chat.setup(opts)
-				require("CopilotChat.integrations.cmp").setup()
-				vim.api.nvim_create_user_command("CopilotChatInline", function(args)
-					chat.ask(args.args, {
-						selection = select.visual,
-						window = {
-							layout = "float",
-							relative = "cursor",
-							width = 1,
-							height = 0.4,
-							row = 1,
-						},
-					})
-				end, { nargs = "*", range = true })
-			end,
-			keys = {
-				{ "<leader>cc", "<cmd>CopilotChatInline<cr>", desc = "CopilotChat - Inline chat" },
-			},
-		},
-	},
-	{
-		"folke/which-key.nvim",
-		dependencies = {
-			"echasnovski/mini.icons",
-			"nvim-tree/nvim-web-devicons",
-		},
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 200
-		end,
-		opts = {},
-	},
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = true,
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-			"hrsh7th/cmp-nvim-lsp",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<C-j>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<C-j>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-
-					["<C-k>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		end,
-	},
+                    ["<C-k>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" }, -- For luasnip users.
+                }, {
+                    { name = "buffer" },
+                }),
+            })
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        end,
+    },
 }
