49ff4c8e7122c7303e4bd731bd4dd99c9ff88d0d
roesch <kevin.roesch@kit.edu>
2025-07-11 10:03:55 +0200

    fix hover

--- lua/plugins/hover.lua
+++ lua/plugins/hover.lua
@@ -16,7 +16,7 @@ return {
                 -- require('hover.providers.highlight')
             end,
             preview_opts = {
-                border = 'single'
+                border = 'rounded'
             },
             -- Whether the contents of a currently open hover window should be moved
             -- to a :h preview-window when pressing the hover keymap.
@@ -31,12 +31,13 @@ return {
         -- Setup keymaps
         -- vim.keymap.set("n", "K", require("hover").hover, {desc = "hover.nvim"})
         -- vim.keymap.set("n", "gK", require("hover").hover_select, {desc = "hover.nvim (select)"})
-        vim.keymap.set("n", "<C-p>", function() require("hover").hover_switch("previous") end, {desc = "hover.nvim (previous source)"})
-        vim.keymap.set("n", "<C-n>", function() require("hover").hover_switch("next") end, {desc = "hover.nvim (next source)"})
+        vim.keymap.set("n", "<C-p>", function() require("hover").hover_switch("previous") end,
+            { desc = "hover.nvim (previous source)" })
+        vim.keymap.set("n", "<C-n>", function() require("hover").hover_switch("next") end,
+            { desc = "hover.nvim (next source)" })
 
         -- Mouse support
         vim.keymap.set('n', '<MouseMove>', require('hover').hover_mouse, { desc = "hover.nvim (mouse)" })
         vim.o.mousemoveevent = true
     end
 }
-
