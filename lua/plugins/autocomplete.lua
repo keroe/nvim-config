ccfb4c7ec80f901f60d15f8ec46a2e609e1ea00c
Kevin Rösch <kevin.roesch@kit.edu>
2026-04-13 17:29:37 +0200

    add blink as autocomplete

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -47,56 +47,76 @@ return {
     },
     {
         "L3MON4D3/LuaSnip",
-        dependencies = {
-            "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets",
-            "hrsh7th/cmp-nvim-lsp",
-        },
+        dependencies = { "rafamadriz/friendly-snippets" },
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
     },
     {
-        "hrsh7th/nvim-cmp",
-        config = function()
-            local cmp = require("cmp")
-            require("luasnip.loaders.from_vscode").lazy_load()
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
+        "saghen/blink.compat",
+        version = "*",
+        opts = {},
+    },
+    { "micangl/cmp-vimtex" },
+    {
+        "saghen/blink.cmp",
+        version = "*",
+        dependencies = {
+            "L3MON4D3/LuaSnip",
+            "saghen/blink.compat",
+            "micangl/cmp-vimtex",
+        },
+        opts = {
+            keymap = {
+                preset = "none",
+                ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+                ["<C-e>"] = { "hide" },
+                ["<CR>"] = { "accept", "fallback" },
+                ["<C-j>"] = { "select_next", "fallback" },
+                ["<C-k>"] = { "select_prev", "fallback" },
+            },
+            snippets = { preset = "luasnip" },
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer" },
+                per_filetype = {
+                    tex = { "vimtex", "snippets", "buffer" },
                 },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
+                providers = {
+                    vimtex = {
+                        name = "vimtex",        -- matches cmp-vimtex source name
+                        module = "blink.compat.source",
+                        transform_items = function(_, items)
+                            local seen = {}
+                            local deduped = {}
+                            for _, item in ipairs(items) do
+                                if not seen[item.label] then
+                                    seen[item.label] = true
+                                    table.insert(deduped, item)
+                                end
+                            end
+                            return deduped
+                        end,
+                    },
                 },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                    ["<C-j>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-
-                    ["<C-k>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                }),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" }, -- For luasnip users.
-                }, {
-                    { name = "buffer" },
-                }),
-            })
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        end,
+            },
+            completion = {
+                documentation = {
+                    auto_show = true,
+                    window = { border = "rounded" },
+                },
+                menu = {
+                    border = "rounded",
+                    draw = {
+                        columns = {
+                            { "label", "label_description", gap = 1 },
+                            { "kind_icon", "source_name" },
+                        },
+                    },
+                },
+            },
+            appearance = {
+                nerd_font_variant = "mono",
+            },
+        },
     },
 }
